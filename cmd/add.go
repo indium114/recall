ea10c11fbd296f2f8b96f32086cc4719e836eb35
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-30 19:09:27 +0200

    feat: Add command

---
+++ cmd/add.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add <name> <priority>",
+	Short: "Add a task",
+	Args:  cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		name := args[0]
+		prio := atoi(args[1])
+
+		if prio < 1 || prio > 3 {
+			color.Red(" Priority must be between 1 and 3")
+			return
+		}
+
+		tasks := loadTasks()
+		used := map[int]bool{}
+
+		for _, t := range tasks {
+			used[t.ID] = true
+		}
+
+		id := 0
+		for used[id] {
+			id++
+		}
+
+		tasks = append(tasks, Task{
+			Name:  name,
+			Prio:  prio,
+			State: false,
+			ID:    id,
+		})
+
+		_ = saveTasks(tasks)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
